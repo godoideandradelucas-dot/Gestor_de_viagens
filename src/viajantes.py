7a7c0223357c4225b6e90ae322002410ab40b507
Lucas Andrade <godoideandradelucas@gmail.com>
2026-04-19 19:04:21 +0100

    Update viajantes.py

--- src/viajantes.py
+++ src/viajantes.py
@@ -1,7 +1,6 @@
-from utils import validar_data, validar_nome, validar_nacionalidade, validar_telefone, validar_email, validar_NIF, validar_interesses, validar_orcamento, isEqual
+from utils import isEqual
 
 viajantes = []
-
 
 def adicionar_viajante(nome, data_nascimento, nacionalidade, telefone, email, NIF, interesses, orcamento):
     viajante = {
@@ -31,12 +30,10 @@ def consultar_viajantes():
 
     print("\n=== CONSULTAR VIAJANTE ===")
     nome = input("Nome do viajante para consultar: ").strip()
-
     if not nome:
         return 400, "Viajante não encontrado."
 
     encontrado = False
-
     for viajante in viajantes:
         if nome.lower() in viajante["nome"].lower():
             for chave, valor in viajante.items():
@@ -49,38 +46,24 @@ def consultar_viajantes():
     return 200, "Sucesso"
 
 
-def atualizar_viajantes():
+def atualizar_viajantes(nome, data_nascimento, nacionalidade, telefone, email, NIF, interesses, orcamento):
     if not viajantes:
         return 404, "Não existem utilizadores registados."
 
-    print("\n=== ATUALIZAR VIAJANTE ===")
-    nome = input("Digite o nome para atualizar o viajante: ").strip()
-
-    if not nome:
+    nome_procurar = input("Digite o nome para atualizar o viajante: ").strip()
+    if not nome_procurar:
         return 400, "Viajante não encontrado."
 
     for viajante in viajantes:
-        if nome.lower() in viajante["nome"].lower():
-            print("Digite os novos dados")
-
-            novo_nome = input("Digite o novo nome:")
-            nova_data = input("Digite a nova data de nascimento(DD/MM/AAAA):")
-            nova_nacionalidade = input("Digite a nova nacionalidade:")
-            novo_telefone = input("Digite o novo telefone:")
-            novo_email = input("Digite o novo email:")
-            novo_NIF = input("Digite o novo NIF:")
-            novos_interesses = input("Digite os novos interesses:")
-            novo_orcamento = input("Digite o novo orçamento(€):")
-
-            viajante["nome"] = validar_nome(novo_nome)
-            viajante["data_nascimento"] = validar_data(nova_data)
-            viajante["nacionalidade"] = validar_nacionalidade(nova_nacionalidade)
-            viajante["telefone"] = validar_telefone(novo_telefone)
-            viajante["email"] = validar_email(novo_email)
-            viajante["NIF"] = validar_NIF(novo_NIF)
-            viajante["interesses"] = validar_interesses(novos_interesses)
-            viajante["orcamento"] = validar_orcamento(novo_orcamento)
-
+        if nome_procurar.lower() in viajante["nome"].lower():
+            viajante["nome"] = nome
+            viajante["data_nascimento"] = data_nascimento
+            viajante["nacionalidade"] = nacionalidade
+            viajante["telefone"] = telefone
+            viajante["email"] = email
+            viajante["NIF"] = NIF
+            viajante["interesses"] = interesses
+            viajante["orcamento"] = orcamento
             return 200, "Sucesso"
     return 404, "Utilizador não encontrado."
 
@@ -91,10 +74,8 @@ def remover_viajantes():
 
     print("\n=== REMOVER VIAJANTE ===")
     nome_remover = input("Nome do viajante para remover: ")
-
     for viajante in viajantes:
         if isEqual(nome_remover, viajante["nome"]):
             viajantes.remove(viajante)
             return 200, "Sucesso"
-
     return 404, "Utilizador não encontrado"
